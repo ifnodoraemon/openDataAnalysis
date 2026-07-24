0edcab5e9faa4f6c661b3e44649d50145cfb5210
ifnodoraemon <ifnodoraemon@gmail.com>
2026-07-24 10:44:56 +0800

    fix(metrics): 为 statusRecorder 实现 http.Hijacker 接口以支持 WebSocket 升级

--- server/metrics/prometheus.go
+++ server/metrics/prometheus.go
@@ -1,6 +1,9 @@
 package metrics
 
 import (
+	"bufio"
+	"fmt"
+	"net"
 	"net/http"
 	"strconv"
 	"time"
@@ -63,6 +66,19 @@ type statusRecorder struct {
 func (r *statusRecorder) WriteHeader(code int) {
 	r.statusCode = code
 	r.ResponseWriter.WriteHeader(code)
+}
+
+func (r *statusRecorder) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	if hj, ok := r.ResponseWriter.(http.Hijacker); ok {
+		return hj.Hijack()
+	}
+	return nil, nil, fmt.Errorf("http.ResponseWriter does not implement http.Hijacker")
+}
+
+func (r *statusRecorder) Flush() {
+	if f, ok := r.ResponseWriter.(http.Flusher); ok {
+		f.Flush()
+	}
 }
 
 // Middleware records Prometheus HTTP request counts and durations.
